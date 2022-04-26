949878f27681b6555f18f67bde136d09c4fd3ce0
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-26 18:23:07 +0000

    update

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -7,10 +8,22 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
+        itemCount: 10,
         itemBuilder: (ctx, index) => Container(
           padding: const EdgeInsets.all(10),
           child: const Text('This works'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/9Ir37jspC6Yi6t5R7TFO/messages')
+              .snapshots()
+              .listen((data) {
+            print(data);
+          });
+        },
       ),
     );
   }
