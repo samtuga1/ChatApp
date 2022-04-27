d48736379f5146b6b0911e5960546be3e265660d
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-27 15:42:17 +0000

    update

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({Key? key}) : super(key: key);
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickedImage;
+  Future<void> pickImage() async {
+    final imagePicker = ImagePicker();
+    final pickedImageFile =
+        await imagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _pickedImage = File(pickedImageFile!.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.grey,
+          radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage!) : null,
+        ),
+        FlatButton.icon(
+          textColor: Theme.of(context).primaryColor,
+          onPressed: pickImage,
+          label: const Text('Add Image'),
+          icon: const Icon(Icons.image),
+        ),
+      ],
+    );
+  }
+}
