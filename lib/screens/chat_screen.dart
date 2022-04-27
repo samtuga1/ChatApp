327c723c6d9a4459d30b107948c88aef635c9cf7
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-27 18:59:43 +0000

    update

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,7 +1,6 @@
 import 'package:chat_app/widgets/messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -14,29 +13,30 @@ class ChatScreen extends StatelessWidget {
         title: const Text('Chat App'),
         actions: [
           DropdownButton(
-              icon: const Icon(Icons.more_vert),
-              items: [
-                DropdownMenuItem(
-                  value: 'logout',
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.exit_to_app,
-                        color: Theme.of(context).iconTheme.color,
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      const Text('Logout')
-                    ],
-                  ),
-                )
-              ],
-              onChanged: (itemIdentifier) {
-                if (itemIdentifier == 'logout') {
-                  FirebaseAuth.instance.signOut();
-                }
-              })
+            icon: const Icon(Icons.more_vert),
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Theme.of(context).iconTheme.color,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Text('Logout')
+                  ],
+                ),
+              )
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          )
         ],
       ),
       body: Column(
