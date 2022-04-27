57a58ec1dcee3147cc97d3881796e9e4653896af
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-27 15:41:55 +0000

    update

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -1,37 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatBubble extends StatelessWidget {
-  const ChatBubble({Key? key, required this.message, required this.isMe})
+  const ChatBubble(
+      {Key? key,
+      required this.username,
+      required this.message,
+      required this.isMe})
       : super(key: key);
   final String message;
   final bool isMe;
+  final String username;
   @override
   Widget build(BuildContext context) {
     return Row(
-        mainAxisAlignment:
-            isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
-                bottomRight: !isMe ? Radius.circular(12) : Radius.circular(0),
-              ),
-            ),
-            width: 140,
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-            margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: Text(
-              message,
-              style: TextStyle(
-                  color: isMe
-                      ? Colors.black
-                      : Theme.of(context).accentTextTheme.headline1!.color),
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+              bottomRight: !isMe ? Radius.circular(12) : Radius.circular(0),
             ),
           ),
-        ]);
+          width: 140,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          child: Column(
+            mainAxisAlignment:
+                isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: [
+              Text(
+                username,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                    color: isMe
+                        ? Colors.black
+                        : Theme.of(context).accentTextTheme.headline1!.color),
+                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
